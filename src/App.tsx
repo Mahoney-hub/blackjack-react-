e2a4cc05ccf9bc3774e7a67be2ec0b4dc67f3236
Pavel Vasilev <pv9998286620@yandex.ru>
2022-02-14 22:30:26 +0300

    added async functions and added states

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,93 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
+interface IImages {
+    png: string
+    svg: string
+}
+
+interface ICard {
+    code: string
+    image: string
+    images: IImages
+    suit: string
+    value: string
+}
+
+interface ICardObj {
+    cards: ICard[]
+    deck_id: string
+    remaining: number
+    success: boolean
+}
+
+interface IDeck {
+    success: boolean
+    deck_id: string
+    shuffled: boolean
+    remaining: boolean
+}
 
 const App = () => {
-    return (
-        <div>
+    // BLL Card
+    const [card, setCard] = useState<string | undefined>(undefined) // текущая карта
+    const [cards, setCards] = useState<string[]>([]) // массив карт на поле
+    const [count, setCount] = useState<number>(0) // счет
+    // BLL Deck
+    const [cardBalance, setCardBalance] = useState<number>(52) // остаток карт в колоде
+    const [deckID, setDeckID] = useState<string>('') // id колоды карт для api
+    useEffect(() => {
+        getDeck()
+    }, [])
 
+    // Functions
+    // Перемешать и получить карты
+    async function getDeck() {
+        try {
+            const response = await axios.get<IDeck>('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+            console.log(response.data)
+            setCardBalance(+(response.data.remaining))
+            setDeckID(response.data.deck_id)
+        } catch (e) {
+            alert(e)
+        }
+    }
+
+    // Нарисовать карту
+    async function getCard(id: string) {
+        const queryString = `https://deckofcardsapi.com/api/deck/${id}/draw/?count=1`
+        try {
+            const response = await axios.get<ICardObj>(queryString)
+            console.log(response.data)
+            const newCard: string = response.data.cards[0].images.png
+            setCard(newCard)
+            setCards([...cards, newCard])
+        } catch (e) {
+            alert(e)
+        }
+    }
+
+    const clearField = () => {
+        setCards([])
+    }
+    // Components before rendering
+    const componentCards = cards.map((c, i) => {
+            return (
+                <img src={c} alt="card" key={i}/>
+            )
+        }
+    )
+    return (
+        <div className={'app'}>
+            <div>
+                {componentCards}
+            </div>
+            <div>
+                <button onClick={() => getCard(deckID)}>click</button>
+            </div>
+            <div>
+                <button onClick={() => clearField()}>clear</button>
+            </div>
         </div>
     );
 };
