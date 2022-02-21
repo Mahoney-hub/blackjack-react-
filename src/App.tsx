0a35d5e98437482d02c3c23d354372e3f3825913
Pavel Vasilev <pv9998286620@yandex.ru>
2022-02-21 22:46:29 +0300

    changed style

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import {getCardCount} from './utils/utils';
+import {Button, ButtonGroup, Paper} from '@mui/material';
 
 interface IImages {
     png: string
@@ -46,7 +47,7 @@ const App = () => {
     async function getDeck() {
         try {
             const response = await axios.get<IDeck>('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
-            console.log(response.data)
+            console.log(response)
             setCardBalance(+(response.data.remaining))
             setDeckID(response.data.deck_id)
         } catch (e) {
@@ -61,7 +62,7 @@ const App = () => {
             const response = await axios.get<ICardObj>(queryString)
             console.log(response.data)
             const newCard: string = response.data.cards[0].images.png
-            setCount(getCardCount(response.data.cards[0].value))
+            setCount(count + getCardCount(response.data.cards[0].value))
             console.log(response.data.cards[0].value)
             setCard(newCard)
             setCards([...cards, newCard])
@@ -72,24 +73,45 @@ const App = () => {
 
     const clearField = () => {
         setCards([])
+        setCount(0)
     }
     // Components before rendering
-    const componentCards = cards.map((c, i) => {
-            return (
-                <img src={c} alt="card" key={i}/>
-            )
-        }
-    )
+    const componentCards = cards.map((c, i) =>
+        <img src={c} alt="card" height="180px" width="110px" key={i}/>)
+    const componentResult = (count < 21) ? count : (count === 21) ? 'BLACKJACK' : 'BOR'
+
     return (
         <div className={'app'}>
-            <div>
-                {componentCards}
+            {/*<div className={'field'}>*/}
+            {/*    <Paper className={'block'}>*/}
+            {/*        {componentCards}*/}
+            {/*    </Paper>*/}
+            {/*    <ButtonGroup variant="contained" fullWidth>*/}
+            {/*        <Button onClick={() => getCard(deckID)}>Еще</Button>*/}
+            {/*        <Button onClick={() => clearField()}>Сброс</Button>*/}
+            {/*        <Button>Three</Button>*/}
+            {/*    </ButtonGroup>*/}
+            {/*    <div className={'count'}>*/}
+            {/*        {componentResult}*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+            <div className={'block block--small'}>
+                <div className={'button'}>hit</div>
             </div>
-            <div>
-                <button onClick={() => getCard(deckID)}>click</button>
+            <div className={'block block--large'}>
+                <div className={'field'}></div>
+                <div className={'field'}></div>
             </div>
-            <div>
-                <button onClick={() => clearField()}>clear</button>
+            <div className={'block block--small'}>
+                <div className={'info'}>
+                    <div>0</div>
+                    <span>Dealer</span>
+                </div>
+                <div className={'button'}>stand</div>
+                <div className={'info'}>
+                    <div>0</div>
+                    <span>Player</span>
+                </div>
             </div>
         </div>
     );
